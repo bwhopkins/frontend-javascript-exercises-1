81f63c9bdb6faae089cdc7a8c4e811cb0eb7ecf1
Brandon Hopkins <brandonwhopkins@gmail.com>
2014-10-18 21:22:50 -0600

    Update challenge.js

--- 02-reading-mocha-tests/00-a-tested-function/challenge.js
+++ 02-reading-mocha-tests/00-a-tested-function/challenge.js
@@ -1,3 +1,3 @@
 module.exports.linkTo = function(text, address) {   
-  return "<a href='" + text + "'>" + address + "</a>"
+  return "<a href='<a href=\'Bloc\'>http://www.bloc.io</a>" + 'to deeply equal' + "<a href=\'http://www.bloc.io\'>Bloc</a>'>" + address + "</a>"
 };
